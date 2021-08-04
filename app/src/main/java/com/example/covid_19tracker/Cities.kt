542b714247767474c0f9339e9ddd461b8cf7806d
Varun Pitty <pittyvarun21@gmail.com>
2021-08-04 13:29:56 +0530

    First Activity Done

---
+++ app/src/main/java/com/example/covid_19tracker/Cities.kt
@@ -0,0 +1,16 @@
+package com.example.covid_19tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cities : AppCompatActivity() {
+
+    companion object{
+        const val State_name = ""
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cities)
+    }
+}
--- app/src/main/java/com/example/covid_19tracker/MainActivity.kt
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.covid_19tracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,10 +9,11 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
-
+    public var state_name : String? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,17 +24,25 @@ class MainActivity : AppCompatActivity() {
             spinner.adapter = adapter
 
             spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                    fun onItemSelected (parent: AdapterView<*> , view : View , position : Int , id : Long){
-                        Toast.makeText(this@MainActivity,"Selected State : " + state[position] , Toast.LENGTH_SHORT  ).show()
-                    }
+                override fun onItemSelected(p0: AdapterView<*>?, view: View, p2: Int, p3: Long) {
+                         state_name = state[p2]
                 }
 
-                override fun onNothingSelected(p0: AdapterView<*>?) {
-                    TODO("Not yet implemented")
+                override fun onNothingSelected(p0: AdapterView<*>?){
                 }
 
             }
     }
 }
+
+    fun Go(view: View) {
+        if(state_name=="Select"){
+            Toast.makeText(this,"Select Your State",Toast.LENGTH_LONG).show()
+        }
+        else{
+            val intent = Intent(this , Cities::class.java)
+            intent.putExtra(Cities.State_name,state_name)
+            startActivity(intent)
+        }
+    }
 }
